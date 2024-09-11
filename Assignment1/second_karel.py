9172219c273faa51ade8d049403d2f6879bf4969
avadwyer <adwyer26@groton.org>
2024-09-11 12:47:21 -0400

    I added code to world one.

---
+++ Assignment1/second_karel.py
@@ -0,0 +1,21 @@
+from stanfordkarel import *
+
+"""
+For this problem, your goal is to
+move the beeper to the designated
+red square. You may use the following
+commands:
+
+turn_left()
+move()
+pick_beeper()
+put_beeper()
+"""
+
+def main():
+    """Karel code goes here!"""
+
+
+if __name__ == "__main__":
+    run_karel_program("second_world")
+    
---
+++ Assignment1/third_karel.py
@@ -0,0 +1,24 @@
+from stanfordkarel import *
+
+"""
+For this problem, the default
+world contains a smily face.
+However, the face has red eyes,
+which makes it look demonic.
+Your goal is to paint the face's
+eye's blue and then move Karel
+back to where it started.
+You may use the following
+functions:
+
+turn_left()
+move()
+paint_corner(BLUE)
+"""
+
+def main():
+    """Karel code goes here!"""
+
+
+if __name__ == "__main__":
+    run_karel_program("third_world")
